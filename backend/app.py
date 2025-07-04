7f2c8901c096f963d1ab2701fcfa051a15786827
srinathnulidonda <srinathnulidonda@gmail.com>
2025-07-04 19:21:12 +0530

    token

--- backend/app.py
+++ backend/app.py
@@ -28,8 +28,8 @@ TMDB_API_KEY = os.environ.get('TMDB_API_KEY', '1cf86635f20bb2aff8e70940e7c3ddd5'
 TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 JIKAN_BASE_URL = 'https://api.jikan.moe/v4'
 ML_SERVICE_URL = os.environ.get('ML_SERVICE_URL', 'https://movies-rec-ml-service.onrender.com')
-TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
-TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '7689567537:AAGvDtu94OlLlTiWpfjSfpl_dd_Osi_2W7c')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '-1002566510721')
 
 # Database initialization
 def init_db():
@@ -135,7 +135,7 @@ def create_admin_user():
     # Admin credentials
     admin_username = "admin"
     admin_email = "admin@movierecommendations.com"
-    admin_password = "AdminPass123!"  # Change this!
+    admin_password = "AdminPass123!"  
     
     try:
         conn.execute('''INSERT INTO users (username, email, password_hash, is_admin) 
